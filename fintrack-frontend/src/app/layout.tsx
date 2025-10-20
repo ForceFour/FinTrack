1836474891ece37030eed890301d4c4b428f04f1
Zayan-Mohamed <itsm.zayan@gmail.com>
2025-10-20 22:06:43 +0530

    feat: Added favicon and added dynamic title for each pages

--- fintrack-frontend/src/app/layout.tsx
+++ fintrack-frontend/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
-import { Inter } from 'next/font/google';
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "FinTrack - AI-Powered Expense Tracker",
@@ -16,6 +16,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+      <link rel="manifest" href="/site.webmanifest" />
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
